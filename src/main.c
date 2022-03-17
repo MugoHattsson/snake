88595c4d9a9815df1bac9966b22e651e9dbdce9d
MugoHattsson <hu5174ma-s@student.lu.se>
2022-03-17 19:35:51 +0100

    Snake now able to take shortcuts on the hamiltonian cycle

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "main.h"
 #include "hamilton.h"
 #include "graphics.h"
@@ -10,6 +11,20 @@
 pos_t *apple;
 
 int board[ARENA_ROWS][ARENA_COLS];
+int buffer = 3;
+
+void print_board()
+{
+    printf("\n");
+    for (int i = 0; i < ARENA_ROWS; i++)
+    {
+        for (int j = 0; j < ARENA_COLS; j++)
+        {
+            printf("%i ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 void grow_snake(snake_t *snake)
 {
@@ -53,29 +68,6 @@ int find_apple()
                 return 1;
 
     return 0;
-}
-
-snake_t *init_snake()
-{
-    snake_t *snake = calloc(1, sizeof(snake_t));
-    pos_t *head = calloc(1, sizeof(pos_t));
-    head->row = ARENA_ROWS / 2;
-    head->col = ARENA_COLS / 2;
-    head->next = NULL;
-    head->prev = NULL;
-    head->dir = RIGHT;
-
-    snake->head = head;
-    snake->tail = head;
-    snake->length = 1;
-    snake->dir = RIGHT;
-
-    board[head->row][head->col] = 1;
-
-    for (int i = 0; i < 5; i++)
-        grow_snake(snake);
-
-    return snake;
 }
 
 int move_snake(snake_t *snake)
@@ -135,6 +127,8 @@ int move_snake(snake_t *snake)
     {
         grow_snake(snake);
         spawn_apple();
+        int progress = (float)snake->length / (float)ARENA_SIZE * 100;
+        printf("%i%%\n", progress);
     }
     if (board[head->row][head->col] == 1)
     {
@@ -145,25 +139,199 @@ int move_snake(snake_t *snake)
     return 1;
 }
 
+direction init_direction(snake_t *snake, int **tour)
+{
+    int row = snake->head->row;
+    int col = snake->head->col;
+
+    int next_tour_num = tour[row][col] == ARENA_ROWS * ARENA_COLS ? 1 : tour[row][col] + 1;
+
+    if (col < ARENA_COLS - 1 && tour[row][col + 1] == next_tour_num)
+        return RIGHT;
+    if (col > 0 && tour[row][col - 1] == next_tour_num)
+        return LEFT;
+    if (row < ARENA_ROWS - 1 && tour[row + 1][col] == next_tour_num)
+        return DOWN;
+    if (row > 0 && tour[row - 1][col] == next_tour_num)
+        return UP;
+}
+
+snake_t *init_snake(int **tour)
+{
+    snake_t *snake = calloc(1, sizeof(snake_t));
+    pos_t *head = calloc(1, sizeof(pos_t));
+    head->row = ARENA_ROWS / 2;
+    head->col = ARENA_COLS / 2;
+    head->next = NULL;
+    head->prev = NULL;
+    head->dir = RIGHT;
+
+    snake->head = head;
+    snake->tail = head;
+    snake->length = 1;
+
+    snake->dir = init_direction(snake, tour);
+
+    board[head->row][head->col] = 1;
+
+    int init_length = 2;
+    for (int i = 0; i < init_length; i++)
+        grow_snake(snake);
+
+    move_snake(snake);
+
+    return snake;
+}
+
+int tour_distance(int a, int b)
+{
+    if (a < b)
+        return b - a - 1;
+    return b - a - 1 + ARENA_SIZE;
+}
+
+int distance_to_apple(snake_t *snake, int **tour)
+{
+    int apple_pos = tour[apple->row][apple->col];
+    int head_pos = tour[snake->head->row][snake->head->col];
+
+    return tour_distance(head_pos, apple_pos);
+}
+
+bool collision(int row, int col)
+{
+    if (row < 0 || row >= ARENA_ROWS || col < 0 || col >= ARENA_COLS)
+        return true;
+
+    return board[row][col] == 1 ? true : false;
+}
+
+bool valid_cut(snake_t *snake, int **tour, int row, int col)
+{
+    if (collision(row, col))
+        return false;
+
+    return true;
+
+    int head = tour[snake->head->row][snake->head->col];
+    int tail = tour[snake->tail->row][snake->tail->col];
+    int next = tour[row][col];
+
+    if (head > tail)
+    {
+        if (next > head && next > tail)
+            return true;
+        if (next < head && next < tail)
+            return true;
+    }
+    if (head < tail && next > head && next < tail)
+        return true;
+
+    return false;
+}
+
+char *direction_to_string(direction dir)
+{
+    switch (dir)
+    {
+    case UP:
+        return "UP";
+    case RIGHT:
+        return "RI";
+    case DOWN:
+        return "DO";
+    case LEFT:
+        return "LE";
+    }
+}
+
+void debug(direction dir, int food_dist, int tail_dist, int cut, int avail)
+{
+    char *label = direction_to_string(dir);
+
+    printf("Dir: %s, Fd: %i, Td: %i, Cd: %i, Ad: %i\n", label, food_dist, tail_dist, cut, avail);
+}
+
 direction next_snake_direction(snake_t *snake, int **tour)
 {
     int row = snake->head->row;
     int col = snake->head->col;
-    int next_tour_num = tour[row][col] == ARENA_ROWS * ARENA_COLS ? 1 : tour[row][col] + 1;
+    const int tour_num = tour[snake->head->row][snake->head->col];
+    const int apple_dist = tour_distance(tour_num, tour[apple->row][apple->col]);
+    const int tail_dist = tour_distance(tour_num, tour[snake->tail->row][snake->tail->col]);
+    const int num_empty_tiles = ARENA_SIZE - snake->length - 1 - 1; // Minus -1 * 2 for snake growth and apple pos
 
-    if (col != ARENA_COLS - 1 && tour[row][col + 1] == next_tour_num)
-        return RIGHT;
-    if (col != 0 && tour[row][col - 1] == next_tour_num)
-        return LEFT;
-    if (row != ARENA_ROWS - 1 && tour[row + 1][col] == next_tour_num)
-        return DOWN;
-    if (row != 0 && tour[row - 1][col] == next_tour_num)
-        return UP;
+    int available_cut_dist = tail_dist - 1 - buffer; // -1 for snake growth, -3 for buffer.
+
+    if (num_empty_tiles < ARENA_SIZE / 2)
+        buffer = 10;
+    if (apple_dist < tail_dist)
+    {
+        available_cut_dist--;
+        if ((tail_dist - apple_dist) * 4 > num_empty_tiles)
+            available_cut_dist -= 10;
+    }
+
+    if (apple_dist < available_cut_dist)
+        available_cut_dist = apple_dist;
+    if (available_cut_dist < 0)
+        available_cut_dist = 0;
+
+    bool can_go_up = valid_cut(snake, tour, row - 1, col);
+    bool can_go_right = valid_cut(snake, tour, row, col + 1);
+    bool can_go_down = valid_cut(snake, tour, row + 1, col);
+    bool can_go_left = valid_cut(snake, tour, row, col - 1);
+
+    direction best_dir;
+    int best_cut = -1;
+
+    if (can_go_left)
+    {
+        int cut = tour_distance(tour_num, tour[row][col - 1]);
+        // debug(LEFT, apple_dist, tail_dist, cut, available_cut_dist);
+        if (cut <= available_cut_dist && cut > best_cut)
+        {
+            best_cut = cut;
+            best_dir = LEFT;
+        }
+    }
+    if (can_go_up)
+    {
+        int cut = tour_distance(tour_num, tour[row - 1][col]);
+        // debug(UP, apple_dist, tail_dist, cut, available_cut_dist);
+        if (cut <= available_cut_dist && cut > best_cut)
+        {
+            best_cut = cut;
+            best_dir = UP;
+        }
+    }
+    if (can_go_right)
+    {
+        int cut = tour_distance(tour_num, tour[row][col + 1]);
+        // debug(RIGHT, apple_dist, tail_dist, cut, available_cut_dist);
+        if (cut <= available_cut_dist && cut > best_cut)
+        {
+            best_cut = cut;
+            best_dir = RIGHT;
+        }
+    }
+    if (can_go_down)
+    {
+        int cut = tour_distance(tour_num, tour[row + 1][col]);
+        // debug(DOWN, apple_dist, tail_dist, cut, available_cut_dist);
+        if (cut <= available_cut_dist && cut > best_cut)
+        {
+            best_cut = cut;
+            best_dir = DOWN;
+        }
+    }
+
+    // printf("Best dir: %s, cut: %i\n", direction_to_string(best_dir), best_cut);
+    return best_dir;
 }
 
 void guide_snake(snake_t *snake, int **tour)
 {
-    pos_t *head = snake->head;
     direction next_dir = next_snake_direction(snake, tour);
 
     switch (next_dir)
@@ -187,22 +355,9 @@ void guide_snake(snake_t *snake, int **tour)
     }
 }
 
-void print_board()
-{
-    for (int i = 0; i < ARENA_ROWS; i++)
-    {
-        for (int j = 0; j < ARENA_COLS; j++)
-        {
-            printf("%i ", board[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
 int has_won(snake_t *snake)
 {
-    if (snake->length == ARENA_COLS * ARENA_ROWS)
+    if (snake->length == ARENA_SIZE)
     {
         printf("You Win!\n");
         return 1;
@@ -235,7 +390,7 @@ int main(int argc, char *argv[])
         return 1;
     int **tour = hamiltonian();
 
-    snake_t *snake = init_snake();
+    snake_t *snake = init_snake(tour);
     srand(time(0));
     spawn_apple();
 
@@ -249,7 +404,7 @@ int main(int argc, char *argv[])
 
         draw_game(snake, apple);
         present_scene();
-        wait(30);
+        wait(16);
     }
 
     cleanup(snake);
--- src/main.h
+++ src/main.h
@@ -1,20 +1,27 @@
-#define SCREEN_WIDTH 720
-#define SCREEN_HEIGHT 720
-#define ARENA_ROWS 6 // Has to be divisible by 2
-#define ARENA_COLS 6
-#define TILE_SIZE 120
+// The number of rows and columns has to be evenly divisible by 2.
+// #define ARENA_ROWS 4
+// #define ARENA_COLS 4
+// #define TILE_SIZE 120
 
-// #define SCREEN_WIDTH 840
-// #define SCREEN_HEIGHT 840
-// #define ARENA_ROWS 14 // Has to be divisible by 2
-// #define ARENA_COLS 14
-// #define TILE_SIZE 60
+// #define ARENA_ROWS 6
+// #define ARENA_COLS 6
+// #define TILE_SIZE 120
 
-// #define SCREEN_WIDTH 900
-// #define SCREEN_HEIGHT 900
-// #define ARENA_ROWS 30 // Has to be divisible by 2
+#define ARENA_ROWS 14
+#define ARENA_COLS 14
+#define TILE_SIZE 60
+
+// #define ARENA_ROWS 30
 // #define ARENA_COLS 30
 // #define TILE_SIZE 30
+
+// #define ARENA_ROWS 34
+// #define ARENA_COLS 64
+// #define TILE_SIZE 30
+
+#define SCREEN_WIDTH ((ARENA_COLS) * (TILE_SIZE))
+#define SCREEN_HEIGHT ((ARENA_ROWS) * (TILE_SIZE))
+#define ARENA_SIZE ((ARENA_ROWS) * (ARENA_COLS))
 
 typedef enum
 {
